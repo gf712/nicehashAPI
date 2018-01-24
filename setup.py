77b9a8a019701cf9d942ea9435da2ab626685851
Gil <g.f.hoben@gmail.com>
2018-01-24 13:33:33 +0000

    gitignore

--- setup.py
+++ setup.py
@@ -16,10 +16,11 @@ setup(
     version=about['__version__'],
     package_dir={'nicehashAPI': 'nicehashAPI'},
     packages=['nicehashAPI'],
-    # url='https://github.com/gf712/PyML',
+    url='https://github.com/gf712/nicehashAPI',
     license=about['__license__'],
     author=about['__author__'],
     author_email=about['__author_email__'],
     description='Nicehash Python API',
-    test_suite="tests", install_requires=['requests']
+    test_suite="tests",
+    install_requires=['requests']
 )
